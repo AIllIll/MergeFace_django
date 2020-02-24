007ca7d76aa2491d7053c25400f721f2b5d02cd4
WyC <864147974@qq.com>
2020-02-24 11:28:22 +0800

    添加素材文件

--- fdsj_backend2/fdsj_backend2/urls.py
+++ fdsj_backend2/fdsj_backend2/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from . import view
 
 urlpatterns = [
     url(r'^merge', view.merge_face),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
